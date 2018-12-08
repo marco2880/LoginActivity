d685fc593e8c2af8ba5755a8d2c5677df33a26ab
estudio <marco2880@gmail.com>
2018-12-08 11:29:24 -0500

    primera modificacion

--- app/src/main/java/com/example/estudio/loginactivity/view/CreateAccountActivity.java
+++ app/src/main/java/com/example/estudio/loginactivity/view/CreateAccountActivity.java
@@ -3,6 +3,8 @@ package com.example.estudio.loginactivity.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.estudio.loginactivity.R;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
