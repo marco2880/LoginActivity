c47779e538528946c746e00c96c12b06234846d3
estudio <marco2880@gmail.com>
2018-12-12 16:06:04 -0500

    se han creado los fragment en blanco si las dos ultimas opciones con el fin de reutilizar el codigo  en el cual crearan tanto las de java y los layout, y los cuales se esta creando en la forma mas limpia y siguien funcionando el sistema

---
+++ app/src/main/java/com/example/estudio/loginactivity/view/ContainerActivity.java
@@ -0,0 +1,15 @@
+package com.example.estudio.loginactivity.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.estudio.loginactivity.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
